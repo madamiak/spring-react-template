6e74a61ef67627200d84effaf59e7357f6b8dc9a
matt <matt@users-MacBook-Pro.local>
2021-10-30 19:32:43 +0200

    Initialize repo

---
+++ backend/src/main/java/com/example/springreacttemplate/SpringReactTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreacttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactTemplateApplication.class, args);
+    }
+
+}
